a6571dd7d1bbdb036bd756699bde1b5ca114e7e7
ayca494 <aycaturkmen46@gmail.com>
2023-01-21 14:01:42 +0300

    Add project files.

---
+++ src/Core/ProductApp.Application/Parameters/RequestParameters.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Application.Parameters
+{
+    public class RequestParameters
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+
+        public RequestParameters(int pageNumber, int pageSize)
+        {
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+        }
+    }
+}
